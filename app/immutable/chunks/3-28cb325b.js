713b9b56f4a85eb3a8b7cf9f734d153cad3f57a6
Andrés <alumno.254958@ies-azarquiel.es>
2023-02-07 18:00:37 +0100

    changes to chunks

--- app/immutable/chunks/3-28cb325b.js
+++ app/immutable/chunks/3-28cb325b.js
@@ -1,1 +1,1 @@
-import{default as t}from"../components/pages/ciberseguridad/page.svelte-06eea958.js";export{t as component};
+import{default as t}from"../components/pages/ciberseguridad/_page.svelte-06eea958.js";export{t as component};
