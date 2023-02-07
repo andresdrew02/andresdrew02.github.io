230e91df1dd99526edad3e20b5344877948ad0e4
Andrés <alumno.254958@ies-azarquiel.es>
2023-02-07 18:16:30 +0100

    recommit

---
+++ _app/immutable/chunks/3-de947396.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/ciberseguridad/_page.svelte-81976c86.js";export{t as component};
---
+++ _app/immutable/chunks/4-9dbba6f0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/desarrollo-videojuegos/_page.svelte-b000b2cd.js";export{t as component};
