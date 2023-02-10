8a1cb0ef830261407e5ab88c1a31b751ae082e1a
Andrés <alumno.254958@ies-azarquiel.es>
2023-02-10 16:58:18 +0100

    fixed footer map responsiveness

---
+++ _app/immutable/chunks/2-eae419a3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-44c29833.js";export{t as component};
---
+++ _app/immutable/chunks/3-4e5b2b03.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/ciberseguridad/_page.svelte-ee1a6581.js";export{t as component};
